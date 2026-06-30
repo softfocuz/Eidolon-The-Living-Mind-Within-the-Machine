ddbddd423931ac7a9226a57b3e1416c34492872d
hintsurnameistan <hint.surnameistan@gmail.com>
2026-06-30 21:23:09 +0800

    feat: add training pipeline runner

---
+++ machine_learning/run.py
@@ -0,0 +1,11 @@
+from load_data import run_load_data
+from features import run_features
+from split import run_split
+from train import run_train
+from saving_model_script import run_saving_model_script
+
+run_load_data()
+run_features()
+run_split()
+run_train()
+run_saving_model_script()
